aa03794c47230be7422b6f3c0f3e90bda466442a
ochukob <akporehweochuko@gmail.com>
2023-08-15 11:51:22 -0700

    annoying

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,22 @@
 include "main.h"
 
 /**
- *  * main - check the code
- *   *
- *    * Return: Always 0.
- *     */
+ *main - check the code
+ *
+ *Return: Always 
+ */
+
 int main(void)
 {
-	    _putchar('_');
-	    _putchar('p');
-	    _putchar('u');
-	    _putchar('t');
-	    _putchar('c');
-	    _putchar('h');
-	    _putcahr('a');
-	    _putchar('r');
-	    _putchar('\n');
-	        return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putcahr('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
 
